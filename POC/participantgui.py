4cbe9103ccb6a22ba75d77dbfc89997025325b30
ryan <167286697+AhSohs@users.noreply.github.com>
2024-06-07 16:20:19 +0800

    Update participantgui.py

--- POC/participantgui.py
+++ POC/participantgui.py
@@ -3,53 +3,50 @@ from pythonosc import udp_client, osc_message_builder
 import time
 
 def send_message2(receiver_ip, receiver_port, address, message):
-	try:
-		# Create an OSC client to send messages
-		client = udp_client.SimpleUDPClient(receiver_ip, receiver_port)
+    try:
+        # Create an OSC client to send messages
+        client = udp_client.SimpleUDPClient(receiver_ip, receiver_port)
 
-		# Send an OSC message to the receiver
-		client.send_message(address, message)
+        # Send an OSC message to the receiver
+        client.send_message(address, message)
 
-		print("MA3 sent successfully.")
-	except:
-		print("MA3 not sent")
+        print("MA3 sent successfully.")
+    except:
+        print("MA3 not sent")
 
 def send_message1(receiver_ip, receiver_port, address, message):
-	try:
-		# Create an OSC client to send messages
-		client = udp_client.SimpleUDPClient(receiver_ip, receiver_port)
+    try:
+        # Create an OSC client to send messages
+        client = udp_client.SimpleUDPClient(receiver_ip, receiver_port)
 
-		# Send an OSC message to the receiver
-		client.send_message(address, message)
+        # Send an OSC message to the receiver
+        client.send_message(address, message)
 
-		print("Reaper sent successfully.")
-	except:
-		print("Reaper not sent")
+        print("Reaper sent successfully.")
+    except:
+        print("Reaper not sent")
 
 def send_message3(receiver_ip, receiver_port, address, message):
-	try:
-		# Create an OSC client to send messages
-		client = udp_client.SimpleUDPClient(receiver_ip, receiver_port)
+    try:
+        # Create an OSC client to send messages
+        client = udp_client.SimpleUDPClient(receiver_ip, receiver_port)
 
-		# Send an OSC message to the receiver
-		client.send_message(address, message)
+        # Send an OSC message to the receiver
+        client.send_message(address, message)
 
-		print("Message sent successfully.")
-	except:
-		print("Message not sent")
+        print("Message sent successfully.")
+    except:
+        print("Message not sent")
 
-
-
-
-#gui
+# GUI
 def start():
     print("This is the start button")
     # Hide the start button and show the other two buttons
     starts.place_forget()
     Replays.place(relx=0.5, rely=0.25, anchor=tk.CENTER)
     Startg.place(relx=0.5, rely=0.75, anchor=tk.CENTER)
-	# REAPER
-    PI_A_ADDR = "192.168.254.30"		# wlan ip
+    # REAPER
+    PI_A_ADDR = "192.168.254.30"        # wlan ip
     PORT = 8000
 
     addr = "/action/41251" # Jump to Marker
@@ -60,9 +57,9 @@ def start():
     send_message1(PI_A_ADDR, PORT, addr, msg)
     send_message3(PI_A_ADDR, PORT, addr2, msg)
 
-    #MA3
+    # MA3
     if __name__ == "__main__":
-        LAPTOP_IP = "192.168.254.30"		# send to laptop w grandMA3
+        LAPTOP_IP = "192.168.254.229"        # send to laptop w grandMA3
         PORTS = 8888                     # laptop w grandMA3 port number
         addrs = "/gma3/cmd"
 
@@ -70,8 +67,8 @@ def start():
 
 def Replay():
     print("Replaying instructions.....")
-	    # REAPER
-    PI_A_ADDR = "192.168.254.30"		# wlan ip
+    # REAPER
+    PI_A_ADDR = "192.168.254.30"        # wlan ip
     PORT = 8000
 
     addr = "/action/41251" # Jump to Marker
@@ -82,17 +79,17 @@ def Replay():
     send_message1(PI_A_ADDR, PORT, addr, msg)
     send_message3(PI_A_ADDR, PORT, addr2, msg)
 
-    #MA3
+    # MA3
     if __name__ == "__main__":
-        LAPTOP_IP = "192.168.254.30"		# send to laptop w grandMA3
+        LAPTOP_IP = "192.168.254.229"        # send to laptop w grandMA3
         PORTS = 8888                     # laptop w grandMA3 port number
         addrs = "/gma3/cmd"
 
         send_message2(LAPTOP_IP, PORTS, addrs, "Go Sequence 24")
 
 def Startgame():
-# REAPER
-    PI_A_ADDR = "192.168.254.30"		# wlan ip
+    # REAPER
+    PI_A_ADDR = "192.168.254.30"        # wlan ip
     PORT = 8000
 
     addr = "/action/41255" # Jump to Marker
@@ -103,9 +100,9 @@ def Startgame():
     send_message1(PI_A_ADDR, PORT, addr, msg)
     send_message3(PI_A_ADDR, PORT, addr2, msg)
 
-    #MA3
+    # MA3
     if __name__ == "__main__":
-        LAPTOP_IP = "192.168.254.30"		# send to laptop w grandMA3
+        LAPTOP_IP = "192.168.254.229"        # send to laptop w grandMA3
         PORTS = 8888                     # laptop w grandMA3 port number
         addrs = "/gma3/cmd"
 
@@ -115,19 +112,19 @@ main = tk.Tk()
 main.geometry("600x400") # size of window
 
 # Buttons
-starts = tk.Button(main, text="Start ▶", font="20", command=start, background="Orange")
-Replays = tk.Button(main, text="Replay ⟳", font="20", command=Replay, background="Orange")
-Startg = tk.Button(main, text="Start The Game! 🥷🏿", font="20", command=Startgame, background="Orange")
+starts = tk.Button(main, text="Start ▶", font=("Helvetica", 24), command=start, background="Orange")
+Replays = tk.Button(main, text="Replay ⟳", font=("Helvetica", 24), command=Replay, background="Orange")
+Startg = tk.Button(main, text="Start The Game! 🥷🏿", font=("Helvetica", 24), command=Startgame, background="Orange")
 
 # Initial Button placement
 starts.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-starts.config(height=5, width=20)
+starts.config(height=10, width=40)
 
 # Initially hide the Replay and Start Game buttons
 Replays.place_forget()
-Replays.config(height=5, width=20)
+Replays.config(height=5, width=40)
 
 Startg.place_forget()
-Startg.config(height=5, width=20)
+Startg.config(height=5, width=40)
 
 main.mainloop()
