8124bdf5ca0bc72673bb2f4e96fe38e33bb13bda
AhSohs <167286697+AhSohs@users.noreply.github.com>
2024-07-19 11:33:13 +0800

    ewfwef

--- NewOscNeo/OurCode.py
+++ NewOscNeo/OurCode.py
@@ -13,8 +13,8 @@ def send_message(receiver_ip, receiver_port, address, message):
         print(f"Message not sent: {e}")
 
 # FOR INFO: IP address and port of the receiving Raspberry Pi
-PI_A_ADDR = "192.168.1.100"  # wlan ip
-PORT = 2000
+PI_A_ADDR = "192.168.254.242"  # wlan ip
+PORT = 2005
 
 # Example usage: sending a regular message
 addr = "/print"
